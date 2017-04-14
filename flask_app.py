0a2f2f5a80030a2c318b5cb781f4817231035c26
Magnus Wahlstrand <magnus.wahlstrand@ericsson.com>
2017-04-14 21:44:57 +0200

    Add collapsable menu

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
 from flask import Flask
+from flask import json, Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     # etc etc, flask app code
-    return 'Konnichiwa, Kanpeki Sekai!'
+    return render_template('main.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', use_reloader=True)
