00d4db4010df00d714a295f750f848622a7fc92e
Magnus Wahlstrand <magnus.wahlstrand@ericsson.com>
2017-04-14 23:29:13 +0200

    Add logo and audio playing ability

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
 
 from flask import Flask
+import glob
 from flask import json, Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+
+@app.route('/list_sounds')
+def play():
+    return render_template('list_audio.html', audio_files=glob.glob('static/audio/*.wav'))
+
+@app.route('/play_sound')
+def play_sound():
+    path = request.args.get('path', None)
+    if path:
+        return render_template('play_audio.html', audio_path=path)
+    else: 
+        return "path argument missing", 404
 
 @app.route('/')
 def home():
