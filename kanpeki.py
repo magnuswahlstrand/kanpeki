30988c70d85ebc06d564d5395ebb52dc6da709c8
Magnus Wahlstrand <magnus.wahlstrand@ericsson.com>
2017-04-14 20:59:11 +0200

    Modify permission and use PythonAnywhere format on flask app

--- kanpeki.py
+++ kanpeki.py
@@ -1,6 +1,13 @@
+#!/usr/bin/env python
+
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
+    # etc etc, flask app code
     return 'Konnichiwa, Kanpeki Sekai!'
+
+if __name__ == '__main__':
+    app.run()
